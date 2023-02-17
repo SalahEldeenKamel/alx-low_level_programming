fb33d0990cb3b7338db7d0df93a0bcddf76a9ffb
SalahEldeenKamel <kamelsalaheldeen@gmail.com>
2023-02-17 05:07:37 -0800

    s

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - Prints a random number and states whether
@@ -7,24 +8,19 @@
  *
  * Return: Always 0.
 */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n = 0)
-	{
-		printf("%d is zero",n):
-	}
-	else if (n > 0)
-        {
-                printf("%d is positive",n):
-        }
+
+	if (n > 0)
+		printf("%d is positive\n", n);
 	else if (n < 0)
-        {
-                printf("%d is negative",n):
-        }
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
 	return (0);
 }
